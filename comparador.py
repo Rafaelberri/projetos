1c2437140c1ff85d7ce757fcff17a17f6543f1c5
Rafaelberri <51866532+Rafaelberri@users.noreply.github.com>
2023-09-24 16:24:41 -0300

    Create comparador.py

---
+++ comparador.py
@@ -0,0 +1,9 @@
+a = int(input("Insira um número: "))
+b = int(input("Insira um número: "))
+
+if a > b:
+    print(f"O número {a} é maior que o número {b}")
+elif a < b:
+    print(f"O número {b} é maior que o número {a}")
+else:
+    print("Erro ao processar o valor informado")
