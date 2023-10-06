073abaa0fc669fa437ec9a2cfe468d6f465fe6fe
Rafaelberri <rafael.bernardino@uni9.edu.br>
2023-10-06 10:34:00 -0300

    Adição do and

---
+++ and.py
@@ -0,0 +1,22 @@
+# O operador lógico AND define a condição quando ambas são true
+
+entrar = input("Digite (E)ntrar oara entrar ou (S)air para sair")
+
+
+if entrar == "E":
+    usuariologin = input("Insira o nome de usuario: ")
+    senhalogin =input("Digite a senha de login: ")
+    usuario = "admin"
+    senha = "123456"
+
+    if usuariologin == usuario and senhalogin == senha:
+
+        print("Bem vindo ao sistema")
+    else: 
+        print("Usuario ou senha incorreto!")
+
+elif entrar == "S":
+    print("Sair")
+
+else:
+    print("Não foi possivel autenticar o sistema")
