b8f26727234e615a30464f4e60a77a388f30d80a
Rafaelberri <rafael.bernardino@uni9.edu.br>
2023-11-13 23:25:58 -0300

    organização dos arquivos

--- list/test.py
+++ list/test.py
@@ -1,5 +1,9 @@
 lista = ['Rafael',22,1.96,'25/03/2001']
-
+lista2 = ['Bernardino', 'março']
 lista.append('solteiro')
 lista.pop(3)
-print(lista)
+lista.append(50)
+lista.extend(lista2)
+nome = lista[0]
+sobrenome = lista2[0]
+print(nome,sobrenome)
