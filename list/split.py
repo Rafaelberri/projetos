c6f60be0d35ec1d7d2f2ad594a4530063b1a6a9d
Rafaelberri <rafael.bernardino@uni9.edu.br>
2023-11-28 08:06:37 -0300

    porra

--- list/split.py
+++ list/split.py
@@ -1,9 +1,19 @@
 #split e join com list e str
-# spli divide uma string
+# split divide uma string
 # join une uma string
 
-frase = "Olha só que coisa, interessante"
+frase = "Olha só que coisa  ,  interessante"
+frase2= "Muito interessante"
 
 lista_palavra = frase.split(',')
+
+
+print(lista_palavra)
+
+
+
+
+for i, frase in enumerate(lista_palavra):
+    lista_palavra[i] = lista_palavra[i].strip() # O strip é uma função que corta os espaços
 
 print(lista_palavra)
