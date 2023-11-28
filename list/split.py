163d1fe137d84cca354ec07584b96f9f7b655618
Rafaelberri <rafael.bernardino@uni9.edu.br>
2023-11-28 07:51:45 -0300

    split

---
+++ list/split.py
@@ -0,0 +1,9 @@
+#split e join com list e str
+# spli divide uma string
+# join une uma string
+
+frase = "Olha só que coisa, interessante"
+
+lista_palavra = frase.split(',')
+
+print(lista_palavra)
