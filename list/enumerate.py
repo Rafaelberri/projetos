fce1bdd6cafa8b83454c26e45d669af0821bbc5d
Rafaelberri <rafael.bernardino@uni9.edu.br>
2023-12-01 02:02:05 -0300

    atividade

--- list/enumerate.py
+++ list/enumerate.py
@@ -4,10 +4,4 @@ lista.append('nome3')
 for codigo, item in enumerate(lista):
     print(codigo, item)
 
-    if codigo == len(lista) - 1:
-        
-        valor1= input('Insira o código do produto a ser verificado: ')
-
-        valorint = int(valor1)
-
-        print(f'O item da lista a ser selecionado é {lista[valorint]}')
+    
---
+++ list/treino.py
@@ -0,0 +1,8 @@
+tupla_nomes = 'Rafael', 'Luis', 'Denis'
+tupla_sobrenomes = 'Bernardino', 'Vinicius', 'Freire'
+tupla_cargos = 'Vendas', 'Suporte', 'Gerente'
+
+
+print(range(tupla_cargos))
+
+    
